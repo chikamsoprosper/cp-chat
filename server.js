bead032a62b57b5358ebf72f990055521c1495ac
Chikamso Prosper <chikamsoprosper2008@gmail.com>
2026-08-17 13:23:56 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,25 +1,9 @@
 const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-
-io.on("connection", (socket) => {
-    console.log("A user connected");
-
-    socket.on("chat message", (msg) => {
-        io.emit("chat message", msg);
-    });
-
-    socket.on("disconnect", () => {
-        console.log("A user disconnected");
-    });
+const PORT = 3000;
+app.get("/", (req, res) => {
+    res.send("CP Chat Backend is Running!");
 });
-
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-    console.log(`CP Chat server running on port ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
